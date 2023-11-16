ce920588d174e58094f7fa55427d31a4ea8d54f5
Amar Husain <amar.husain156@gmail.com>
2023-11-16 11:07:25 +0530

    deploy 2

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,36 +7,24 @@ const JWT_KEY = process.env.JWT_KEY || '';
 const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 8080;
 const MODEL_API_URL = process.env.MODEL_API_URL || '';
 
-let MONGO_USERNAME = '';
-let MONGO_PASSWORD = '';
 let MONGO_URI = '';
-let DB_NAME = '';
 
 
 if (process.env.NODE_ENV === 'test') {
-    MONGO_USERNAME = process.env.MONGO_USERNAME_TEST || '';
-    MONGO_PASSWORD = process.env.MONGO_PASSWORD_TEST || '';
     MONGO_URI = process.env.MONGO_URI_TEST || '';
-    DB_NAME = process.env.DB_NAME__TEST || '';
 }
 
 if (process.env.NODE_ENV === 'development') {
-    MONGO_USERNAME = process.env.MONGO_USERNAME_DEVELOPMENT || '';
-    MONGO_PASSWORD = process.env.MONGO_PASSWORD_DEVELOPMENT || '';
     MONGO_URI = process.env.MONGO_URI_DEVELOPMENT || '';
-    DB_NAME = process.env.DB_NAME_DEVELOPMENT || '';
 }
 
 if (process.env.NODE_ENV === 'production') {
-    MONGO_USERNAME = process.env.MONGO_USERNAME_PRODUCTION || '';
-    MONGO_PASSWORD = process.env.MONGO_PASSWORD_PRODUCTION || '';
-    MONGO_URI = process.env.MONGO_URI_PRODUCTION || '';
-    DB_NAME = process.env.DB_NAME_PRODUCTION || '';
+    MONGO_URI = process.env.AZURE_COSMOS_CONNECTIONSTRING || '';
 }
 
 export const config: Config = {
     mongo: {
-        uri: MONGO_URI + '/' + DB_NAME
+        uri: MONGO_URI
     },
     server: {
         port: SERVER_PORT
